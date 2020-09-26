27603b8051d3adc14e13496c072575f9a5eafb9f
JulioAvalos <julioavalos2@msn.com>
2020-09-25 21:25:27 -0600

    Refactorizando los reducers de pokemon

--- src/store/reducers/pokemons.js
+++ src/store/reducers/pokemons.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../shared/utility';
 
 const initialState = {
     pokemons: [],
@@ -9,49 +10,42 @@ const initialState = {
     loading: true
 };
 
+const setPage = (state, action)  => {
+    return updateObject(state, {
+        pokemons: action.pokemons,
+        prevPageUrl: action.prevPageUrl,
+        nextPageUrl: action.nextPageUrl,
+        error: false
+    });
+};
+
+const pokemonSelected = (state, action) => {
+    return updateObject(state, {
+        pokemon: {}
+    })
+}
+
+const fetchFailed = (state, action) => {
+    return updateObject(state, {
+        error: true
+    })
+}
+
+const fetchPokemonDetails = (state, action) => {
+    return updateObject(state, {
+        pokemon: action.pokemon
+    })
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case actionTypes.SET_PREV_PAGE:
-            return {
-                ...state,
-                pokemons: action.pokemons,
-                prevPageUrl: action.prevPageUrl,
-                nextPageUrl: action.nextPageUrl,
-                error: false
-            };
-        case actionTypes.SET_NEXT_PAGE:
-            return {
-                ...state,
-                pokemons: action.pokemons,
-                prevPageUrl: action.prevPageUrl,
-                nextPageUrl: action.nextPageUrl,
-                error: false
-            };
-        case actionTypes.FETCH_INIT_POKEMONS: 
-            return {
-                ...state,
-                pokemons: action.pokemons,
-                prevPageUrl: action.prevPageUrl,
-                nextPageUrl: action.nextPageUrl,
-                error: false
-            };
-        case actionTypes.SELECTED: 
-            return {
-                ...state,
-                pokemon: {}
-            };
-        case actionTypes.FETCH_POKEMONS_FAILED: 
-            return {
-                ...state,
-                error: true
-            };
-        case actionTypes.FETCH_POKEMON_DETAILS:
-            return {
-                ...state,
-                pokemon: action.pokemon
-            }
-        default:
-            return state;
+        case actionTypes.SET_PREV_PAGE: return setPage(state, action);
+        case actionTypes.SET_NEXT_PAGE: return setPage(state, action);
+        case actionTypes.FETCH_INIT_POKEMONS: return setPage(state, action);
+        case actionTypes.SELECTED: return pokemonSelected(state, action);
+        case actionTypes.FETCH_POKEMONS_FAILED: return fetchFailed(state, action);
+        case actionTypes.FETCH_POKEMON_DETAILS: return fetchPokemonDetails(state, action);
+        default: return state;
     }
 }
 
