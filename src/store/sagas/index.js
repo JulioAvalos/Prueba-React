9d5a560afe041729731907250df3bdb0c7604275
JulioAvalos <julioavalos2@msn.com>
2020-09-26 00:59:02 -0600

    Refactorizando sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,15 @@
-import { takeEvery } from 'redux-saga/effects';
+import {
+    takeEvery,
+    all,
+    takeLatest
+} from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 
-import { 
-    addFavoritePokemonSaga, 
-    fetchFavoritePokemonsSaga, 
-    removeFavoritePokemonsSaga 
+import {
+    addFavoritePokemonSaga,
+    fetchFavoritePokemonsSaga,
+    removeFavoritePokemonsSaga
 } from './favorites';
 
 import {
@@ -15,15 +19,17 @@ import {
     fetchPokemonDetailsSaga
 } from './pokemons';
 
-export function* watchFavorites () {
-    yield takeEvery(actionTypes.ADD_INIT_FAVORITE_POKEMON, addFavoritePokemonSaga);
-    yield takeEvery(actionTypes.FETCH_INIT_FAV_POKEMONS, fetchFavoritePokemonsSaga);
-    yield takeEvery(actionTypes.INIT_REMOVE_FAVORITE_POKEMON, removeFavoritePokemonsSaga);
+export function* watchFavorites() {
+    yield all([
+        takeEvery(actionTypes.ADD_INIT_FAVORITE_POKEMON, addFavoritePokemonSaga),
+        takeEvery(actionTypes.FETCH_INIT_FAV_POKEMONS, fetchFavoritePokemonsSaga),
+        takeEvery(actionTypes.INIT_REMOVE_FAVORITE_POKEMON, removeFavoritePokemonsSaga)
+    ]);
 }
 
-export function* watchPokemons () {
-    yield takeEvery(actionTypes.INIT_SET_NEXT_PAGE, goToPrevPageSaga);
-    yield takeEvery(actionTypes.INIT_SET_PREV_PAGE, goToNextPageSaga);
+export function* watchPokemons() {
+    yield takeLatest(actionTypes.INIT_SET_NEXT_PAGE, goToPrevPageSaga);
+    yield takeLatest(actionTypes.INIT_SET_PREV_PAGE, goToNextPageSaga);
     yield takeEvery(actionTypes.INIT_FETCH_POKEMONS, initPokemonsSaga);
     yield takeEvery(actionTypes.INIT_FETCH_POKEMON_DETAILS, fetchPokemonDetailsSaga);
 }
